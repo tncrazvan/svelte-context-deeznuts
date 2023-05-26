f8228f3f67fc376d30f66f99173a32bf20f13304
tncrazvan <tangent.jotey@gmail.com>
2023-05-27 01:48:00 +0200

    showcasing 2 different lists

--- src/lib/preview/app.svelte
+++ src/lib/preview/app.svelte
@@ -3,6 +3,18 @@
   import List from './list.svelte'
 </script>
 
+<h3>List 1</h3>
+<List>
+  <Item>item 1</Item>
+  <Item>item 2</Item>
+  <Item>item 3</Item>
+  <Item>item 4</Item>
+  <Item>item 5</Item>
+</List>
+
+<hr />
+
+<h3>List 2</h3>
 <List>
   <Item>item 1</Item>
   <Item>item 2</Item>
